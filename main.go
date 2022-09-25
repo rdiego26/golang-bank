23746952d7547895fc961d8054b9ba2ff76eda12
ramos <diego.ramos@pipefy.com>
2022-09-25 12:54:57 +0100

    feature: implement withdraw method

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type Account struct {
 	holderName    string
@@ -9,7 +12,21 @@ type Account struct {
 	balance       float64
 }
 
-func main() {
+func (account *Account) withdraw(requestedValue float64) string {
+	time.Sleep(2 * time.Second)
+
+	allowed := requestedValue > 0 && requestedValue <= account.balance
+	if allowed {
+		account.balance -= requestedValue
+		fmt.Println("Balance after withdraw -> ", account.balance)
+		return "Withdrawal made successfully, thanks for using our services!"
+	} else {
+		fmt.Println("Your balance -> ", account.balance)
+		return "Insufficient funds, we couldn't achieve your request!"
+	}
+}
+
+func namedParameters() {
 	var holderName string = "Ramos"
 	var branchCode int = 589
 	var accountNumber int = 123456
@@ -17,6 +34,11 @@ func main() {
 
 	newAccount := Account{balance: 100.00}
 
+	fmt.Println(newAccount)
+	fmt.Println(Account{holderName, accountNumber, branchCode, balance})
+}
+
+func comparisonTypes() {
 	var crisAccount *Account
 	crisAccount = new(Account)
 	crisAccount.holderName = "Cris"
@@ -27,8 +49,6 @@ func main() {
 	crisAccount2.holderName = "Cris"
 	crisAccount2.balance = 500
 
-	fmt.Println(newAccount)
-	fmt.Println(Account{holderName, accountNumber, branchCode, balance})
 	fmt.Println(*crisAccount)
 
 	// Comparing references(not values)
@@ -36,5 +56,17 @@ func main() {
 
 	// Comparing values
 	fmt.Println("*crisAccount == *crisAccount2 ->", *crisAccount == *crisAccount2)
+
+}
+
+func main() {
+	newAccount := Account{"Ramos", 123, 9837, 30.5}
+	fmt.Println("Initial balance -> ", newAccount.balance)
+
+	fmt.Println("Trying withdraw operation with 13...")
+	fmt.Println(newAccount.withdraw(13))
+	fmt.Println("")
+	fmt.Println("Trying withdraw operation with 130...")
+	fmt.Println(newAccount.withdraw(130))
 
 }
